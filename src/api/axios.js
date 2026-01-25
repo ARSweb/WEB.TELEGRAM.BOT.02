d6938ff2c3c2a58fcdf0b5d994c3f59de0403074
Azizbek <asausvivobookbyazizbek@gmail.com>
2026-01-25 16:36:38 +0500

    use ngrok for backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://lailah-pseudophilosophical-cloddishly.ngrok-free.dev "
 })
 export default api

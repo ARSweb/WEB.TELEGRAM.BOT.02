eca7d103579e074064a3c7a6538f3b7f00b13d93
Azizbek <asausvivobookbyazizbek@gmail.com>
2026-01-28 10:44:13 +0500

    re deploy for teacher

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "https://webtelegrambot02server-production.up.railway.app"
+    baseURL: "https://vigilant-acceptance-production-1b9b.up.railway.app"
 })
 export default api

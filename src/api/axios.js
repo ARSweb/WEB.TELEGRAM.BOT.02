1beabfa1b228f067907c09dfd794277b7b4044ea
Azizbek <asausvivobookbyazizbek@gmail.com>
2026-01-28 11:03:17 +0500

    local deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "https://vigilant-acceptance-production-1b9b.up.railway.app"
+    baseURL: "https://lailah-pseudophilosophical-cloddishly.ngrok-free.dev"
 })
 export default api
